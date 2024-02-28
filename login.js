05db068a5cff80ca298fb7be61ff7d64091c66dc
Parker <parkersam@gmail.com>
2024-02-27 19:16:30 -0700

    Login now denies blank username and displays warning to user

--- login.js
+++ login.js
@@ -1,6 +1,11 @@
 function login() {
     const nameEl = document.querySelector("#name");
-    localStorage.setItem("userName", nameEl.value);
-    window.location.href = "play.html";
+    if(nameEl.value === ""){
+      document.getElementById("desc").innerHTML = "Invalid username";
+      document.getElementById("desc").style.color = "red";
+    } else {
+      localStorage.setItem("userName", nameEl.value);
+      window.location.href = "home.html";
+    }
   }
   
