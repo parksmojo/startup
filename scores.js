8be61c5ff89200b4b60c378c42a706c9cfcc80c3
Parker <parkersam@gmail.com>
2024-02-28 00:00:38 -0700

    Reorganized display code into new findFavorite() function

--- scores.js
+++ scores.js
@@ -3,6 +3,7 @@ class ScoreKeep {
 
     constructor(){
         this.myStats = { games: 5, wins: 3, losses: 2, guns: 6, rock: 2, paper: 3, scissors: 4 }
+        this.display()
     }
     
     display(){
@@ -11,16 +12,19 @@ class ScoreKeep {
         document.getElementById("losses").textContent = this.myStats.losses;
         document.getElementById("gun").textContent = this.myStats.guns;
 
+        const favorite = this.findFavorite();
+        document.getElementById("fav").textContent = favorite;
+    }
+
+    findFavorite(){
         const items = new Map([
             [this.myStats.guns, "Gun"],
             [this.myStats.rock, "Rock"],
             [this.myStats.paper, "Paper"],
             [this.myStats.scissors, "Scissors"]
         ]);
-        const favorite = items.get(Math.max(...items.keys()))
-        document.getElementById("fav").textContent = favorite;
+        return items.get(Math.max(...items.keys()))
     }
 }
 
 const scoring = new ScoreKeep()
-scoring.display()
