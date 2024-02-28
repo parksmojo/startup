1b109fa67c7dbe6b63ae2ff7b7b2396eea3e93fc
Parker <parkersam@gmail.com>
2024-02-28 16:20:47 -0700

    Streamlined username insertion into the header

--- login.js
+++ login.js
@@ -1,15 +1,13 @@
-insertUsername()
-
 function login() {
-    const nameEl = document.querySelector("#name");
-    if(nameEl.value === ""){
-      document.getElementById("desc").innerHTML = "Invalid username";
-      document.getElementById("desc").style.color = "red";
-    } else {
-      localStorage.setItem("userName", nameEl.value);
-      window.location.href = "home.html";
-    }
+  const nameEl = document.querySelector("#name");
+  if(nameEl.value === ""){
+    document.getElementById("desc").innerHTML = "Invalid username";
+    document.getElementById("desc").style.color = "red";
+  } else {
+    localStorage.setItem("userName", nameEl.value);
+    window.location.href = "home.html";
   }
+}
   
 function logout() {
   localStorage.removeItem("userName");
