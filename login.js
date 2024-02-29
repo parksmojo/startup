c880547eecd55a656bbe9254831fcff62243e4d4
Parker <parkersam@gmail.com>
2024-02-28 18:54:03 -0700

    Fixed and reworked usage of localStorage - re-implemented accessing storage

--- login.js
+++ login.js
@@ -4,18 +4,18 @@ function login() {
     document.getElementById("desc").innerHTML = "Invalid username";
     document.getElementById("desc").style.color = "red";
   } else {
-    localStorage.setItem("userName", nameEl.value);
+    localStorage.setItem("currentUser", nameEl.value);
     window.location.href = "home.html";
   }
 }
   
 function logout() {
-  localStorage.removeItem("userName");
+  localStorage.removeItem("currentUser");
 }
 
 function insertUsername(){
   const userEl = document.getElementById("user");
   if(userEl !== null){
-    userEl.textContent = localStorage.getItem('userName');
+    userEl.textContent = localStorage.getItem('currentUser');
   }
 }
--- scores.js
+++ scores.js
@@ -1,50 +1,62 @@
 class ScoreKeep {
-    myStats;
-
     constructor(){
-        this.myStats = this.loadStats();
-        this.display();
-    }
-    
-    display(){
-        document.getElementById("games").textContent = this.myStats.games;
-        document.getElementById("wins").textContent = this.myStats.wins;
-        document.getElementById("losses").textContent = this.myStats.losses;
-        document.getElementById("gun").textContent = this.myStats.guns;
-
-        const favorite = this.findFavorite();
-        document.getElementById("fav").textContent = favorite;
+        const currentUser = localStorage.getItem('currentUser') ?? 'User';
+        const currentUserStats = this.getStat(currentUser);
+        this.display(currentUserStats);
     }
 
-    findFavorite(){
-        if(this.myStats.games === 0){
-            return "None";
-        }
-
-        const items = new Map([
-            [this.myStats.guns, "Gun"],
-            [this.myStats.rock, "Rock"],
-            [this.myStats.paper, "Paper"],
-            [this.myStats.scissors, "Scissors"]
-        ]);
-        return items.get(Math.max(...items.keys()))
-    }
-
-    saveStats(stats = this.myStats){
-        localStorage.setItem("userStats", JSON.stringify(stats));
-    }
-
-    loadStats(){
-        const statsText = localStorage.getItem('userStats');
-        if (statsText) {
-            return JSON.parse(statsText);
+    getStat(currentUser = "User"){
+        console.log("getting stats for", currentUser);
+        const statMap = this.loadStats();
+        if(statMap.has(currentUser)){
+            console.log(currentUser, "has stats");
+            return statMap.get(currentUser);
         } else {
+            console.log(currentUser, "is a new user");
             const defaultStats = { games: 0, wins: 0, losses: 0, guns: 0, rock: 0, paper: 0, scissors: 0 };
-            this.saveStats(defaultStats)
+            statMap.set(currentUser,defaultStats);
             return defaultStats;
         }
     }
 
+    loadStats(){
+        console.log("loading stat map");
+        const statsText = localStorage.getItem('localStatMap');
+        if(statsText.length > 2){
+            console.log("found map")
+            const statsMap = new Map(Object.entries(JSON.parse(statsText)));
+            return statsMap;
+        } else {
+            console.log("creating new map");
+            const defaultStats = { games: 0, wins: 0, losses: 0, guns: 0, rock: 0, paper: 0, scissors: 0 };
+            const newStatsMap = new Map([[this.currentUser, defaultStats]]);
+            return newStatsMap;
+        }
+    }
+
+    display(stats){
+        document.getElementById("games").textContent = stats.games;
+        document.getElementById("wins").textContent = stats.wins;
+        document.getElementById("losses").textContent = stats.losses;
+        document.getElementById("gun").textContent = stats.guns;
+
+        const favorite = this.findFavorite(stats);
+        document.getElementById("fav").textContent = favorite;
+    }
+
+    findFavorite(stats){
+        if(stats.games === 0){
+            return "None";
+        }
+
+        const items = new Map([
+            [stats.guns, "Gun"],
+            [stats.rock, "Rock"],
+            [stats.paper, "Paper"],
+            [stats.scissors, "Scissors"]
+        ]);
+        return items.get(Math.max(...items.keys()))
+    }
 }
 
 const scoring = new ScoreKeep();
