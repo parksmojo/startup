8ee81f04ccec1900f7c72068ffdfd7ac2e901ddc
Parker <parkersam@gmail.com>
2024-02-28 20:49:30 -0700

    Created play.js and added some starter code

---
+++ play.js
@@ -0,0 +1,16 @@
+class Game {
+    pickRock(){
+        console.log("picked rock");
+    }
+    pickPaper(){
+        console.log("picked paper");
+    }
+    pickScissors(){
+        console.log("picked scissors");
+    }
+    pickGun(){
+        console.log("picked gun");
+    }
+}
+
+const game = new Game();
