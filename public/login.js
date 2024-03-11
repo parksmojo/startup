884b36685b8216adad2c5d93099dc45310bf6838
Parker <parkersam@gmail.com>
2024-03-10 20:04:29 -0600

    Added quote 3rd party api

--- public/login.js
+++ public/login.js
@@ -1,16 +1,12 @@
 function login() {
   const nameEl = document.querySelector("#name");
-  if(nameEl.value === "" || checkName(nameEl.value)){
+  if(nameEl.value === ""){
     document.getElementById("desc").innerHTML = "Invalid username";
     document.getElementById("desc").style.color = "red";
   } else {
     localStorage.setItem("currentUser", nameEl.value);
     window.location.href = "home.html";
   }
-}
-
-function checkName(name){
-  return name === "bad";
 }
   
 function logout() {
@@ -22,4 +18,23 @@ function insertUsername(){
   if(userEl !== null){
     userEl.textContent = localStorage.getItem('currentUser');
   }
+}
+
+function displayQuote(data) {
+  fetch('https://api.quotable.io/random')
+    .then((response) => response.json())
+    .then((data) => {
+      const containerEl = document.querySelector('#quote');
+
+      const quoteEl = document.createElement('p');
+      quoteEl.classList.add('quote');
+      const authorEl = document.createElement('p');
+      authorEl.classList.add('author');
+
+      quoteEl.textContent = data.content;
+      authorEl.textContent = data.author;
+
+      containerEl.appendChild(quoteEl);
+      containerEl.appendChild(authorEl);
+    });
 }
