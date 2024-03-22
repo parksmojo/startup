be4f113d0aaf25894f748f42827db24f3a4634bf
Parker <parkersam@gmail.com>
2024-03-21 21:29:23 -0600

    Implemented login with database

--- public/login.js
+++ public/login.js
@@ -1,16 +1,63 @@
 function login() {
+  console.log("Logging in");
   const nameEl = document.querySelector("#name");
-  if(nameEl.value === ""){
-    document.getElementById("desc").innerHTML = "Invalid username";
+  const passwordEl = document.querySelector("#password");
+  if(nameEl.value === "" || passwordEl.value === ""){
+    document.getElementById("desc").innerHTML = "Invalid username or password";
     document.getElementById("desc").style.color = "red";
   } else {
-    localStorage.setItem("currentUser", nameEl.value);
-    window.location.href = "home.html";
+    loginOrCreate(nameEl.value);
+  }
+}
+
+async function loginOrCreate(username) {
+  let endpoint = "";
+  const theuser = await getUser(username);
+  console.log("the user found is: " + theuser);
+  if(theuser === null){
+    console.log("New user");
+    endpoint = `/api/auth/create`;
+  } else {
+    console.log("Existing user");
+    endpoint = `/api/auth/login`;
+  }
+
+  const userName = document.querySelector('#name')?.value;
+  const password = document.querySelector('#password')?.value;
+  const response = await fetch(endpoint, {
+    method: 'post',
+    body: JSON.stringify({ email: userName, password: password }),
+    headers: {
+      'Content-type': 'application/json; charset=UTF-8',
+    },
+  });
+
+  if (response.ok) {
+    localStorage.setItem("currentUser", userName);
+    window.location.href = 'home.html';
+  } else {
+    const body = await response.json();
+    document.getElementById("desc").innerHTML = "Invalid Login";
+    document.getElementById("desc").style.color = "red";
   }
 }
   
 function logout() {
   localStorage.setItem("currentUser","User");
+  fetch(`/api/auth/logout`, {
+    method: 'delete',
+  });
+}
+
+async function getUser(email) {
+  let scores = [];
+  // See if we have a user with the given email.
+  const response = await fetch(`/api/user/${email}`);
+  if (response.status === 200) {
+    return response.json();
+  }
+
+  return null;
 }
 
 function insertUsername(){
