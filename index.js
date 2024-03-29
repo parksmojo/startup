211e31db2e97720b75101f5f421b408ce223a25b
Parker <parkersam@gmail.com>
2024-03-29 16:38:30 -0600

    Setup index.js to use websocket

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const bcrypt = require('bcrypt');
 const express = require('express');
 const app = express();
 const DB = require('./database.js');
+const { peerProxy } = require('./peerProxy.js');
 
 const authCookieName = 'token';
 
@@ -121,8 +122,9 @@ function setAuthCookie(res, authToken) {
     });
 }
 
-app.listen(port, () => {
+const httpService = app.listen(port, () => {
     console.log(`Listening on port ${port}`);
 });
 
+peerProxy(httpService);
 let scores = new Map();
