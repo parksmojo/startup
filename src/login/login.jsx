8ecef493819bd8841049062d5ba5f1610de3669a
Parker <parkersam@gmail.com>
2024-04-12 19:07:51 -0600

    Login HTML inserted

--- src/login/login.jsx
+++ src/login/login.jsx
@@ -1,9 +1,18 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { loginFunc } from "./loginFuncs";
 
 export function Login() {
   return (
     <main className='light'>
-      <div>login displayed here</div>
+      <div className="popup">
+        <h1>Login to play</h1>
+        <input type="text" id="name" placeholder="Username" />
+        <input type="text" id="password" placeholder="Password" />
+        <Link to="/home"><button className="buttons" type="submit" onClick={() => loginFunc()}>Login</button></Link>
+        <p id="desc"></p>
+        <div id="quote"></div>
+      </div>
     </main>
   );
 }
